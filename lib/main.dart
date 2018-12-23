cabdf1c36cfc38b699724582a9e0b2c7ffb22bd5
姚巍 <15919688564@163.com>
2018-12-23 22:02:31 +0800

    add mac os run notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'home/home_screen.dart';
 import 'constants.dart' show AppColors;
+
+ // mac os 运行需要
 import 'package:flutter/foundation.dart';
 void main() {
-  debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+  // mac os 运行需要
+  // debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown
